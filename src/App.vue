0649f3e7696f73f3c02a8e66e42c651f9215cd93
CarloCassini <c.cassini87@gmail.com>
2023-09-19 12:56:00 +0200

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+// importo lo store
+import { store } from "./data/store";
+
+// importo i componenti
+// import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  // components: { AppHeader },
+
+  data() {
+    return {
+      store,
+    };
+  },
+
+  computed: {},
+
+  methods: {},
+
+  // created(): {},
+
+  props: {},
+
+  emits: [],
+};
+</script>
+
+<template>
+  <h1>
+    hey oh
+    <div class="m-5">saaa</div>
+    let's go
+  </h1>
+  <font-awesome-icon icon="fa-solid fa-paper-plane" />
+  {{ store.prova }}
+</template>
+
+<style lang="scss">
+h1 {
+  color: red;
+  div {
+    color: blue;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// importo il css di bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// importo le regole javascript di bootstap
+import * as bootstrap from "bootstrap";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* import specific icons */
+// (ccn.ADD)
+// questa parte importa una icona specifica. io scelgo di importare la busta della mailce il paperPlane
+// questa operazione è da fare per ongni oggetto si FontAwesome
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+// ogni icona aggiunta alla libreria (ccn.ADD) va aggiunta a questa riga di co-dice.
+library.add(faEnvelope, faPaperPlane);
+// per usare font awerome devo agganciarmi a un componente al quale aggiungerò tutti gli elementi che mi serviranno
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
