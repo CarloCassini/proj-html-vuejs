83e26a9a97b4f0e0e9886cc30bcd83f1e5b31f95
CarloCassini <c.cassini87@gmail.com>
2023-09-19 18:31:42 +0200

    start hero

--- src/components/main components/home.vue
+++ src/components/main components/home.vue
@@ -21,11 +21,39 @@ export default {
 </script>
 
 <template>
-  <img :src="backgrounImage" alt="" />
+  <section>
+    <div class="container">
+      <div class="row hero-zone">
+        <div class="col-4">
+          <div class="intro-title">fresh and tasty backery every day</div>
+          <h2>the perfect fresh bread</h2>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            velit fugit iure, necessitatibus blanditiis maxime esse perferendis
+            reiciendis earum atque dicta officia tempora sunt reprehenderit ut
+            hic a ipsam et.
+          </div>
+          <div>XXX BOTTONE XXX</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- <img :src="backgrounImage" alt="" /> -->
 </template>
 
 <style lang="scss" scoped>
+section {
+  background-size: cover;
+  background-image: url("images/backgrounds/hero-header-bakery.jpg");
+
+  .hero-zone {
+    padding: 200px 0;
+  }
+}
 img {
   width: 100%;
+}
+.intro-title {
+  color: grey;
 }
 </style>
