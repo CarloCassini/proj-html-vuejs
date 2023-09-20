70b2fb23d93634f7392d6abc83db11d437ce0a78
CarloCassini <c.cassini87@gmail.com>
2023-09-20 11:35:39 +0200

    add selection tu header

--- src/data/store.js
+++ src/data/store.js
@@ -5,13 +5,18 @@ import { reactive } from "vue";
 // qui dentro mettimao tutti i data globali
 export const store = reactive({
   siteSections: [
-    "Home",
-    "Shop",
-    "About",
-    "Gallery",
-    "Location",
-    "Journal",
-    "Contact",
-    "My Account",
+    { index: "Home", active: true, icon: false },
+    { index: "Shop", active: false, icon: false },
+    { index: "About", active: false, icon: false },
+    { index: "Gallery", active: false, icon: false },
+    { index: "Location", active: false, icon: false },
+    { index: "Journal", active: false, icon: false },
+    { index: "Contact", active: false, icon: false },
+    { index: "My Account", active: false, icon: false },
+    {
+      index: "fa-solid fa-cart-shopping",
+      active: false,
+      icon: true,
+    },
   ],
 });
